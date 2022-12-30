765a2f25b221e0bf268392c87970179bb7c50dbe
bionikas2 <bionikas2@gmail.com>
2022-12-30 16:17:32 +0200

    Blog page added

--- portfolio-project/portfolioApp/models.py
+++ portfolio-project/portfolioApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    titles = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to="portfolioApp/images/")
     url = models.URLField(blank=True)
