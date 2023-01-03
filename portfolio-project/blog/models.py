a7782ff4ebb18aa0d0fffec5947eab465292ca3e
bionikas2 <bionikas2@gmail.com>
2023-01-03 14:28:44 +0200

    Blogs
    Shows blogs

--- portfolio-project/blog/models.py
+++ portfolio-project/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
+    text = models.TextField()
