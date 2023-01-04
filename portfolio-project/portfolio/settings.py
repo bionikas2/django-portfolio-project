5ee9796aa7c188d7a4a72d9d07fb94e571599ce5
Mantas <bionikas2@gmail.com>
2023-01-04 18:20:33 +0000

    Deleted old files

--- portfolio-project/portfolio/settings.py
+++ portfolio-project/portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(rwis9_&!h)np@vfd9q5(b_futb_15as-4nlpf^w!8t9z9wta%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bionikas2.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
